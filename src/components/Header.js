793b42da7cb170549ada51bfb6a404bed1e07ff2
bookhabit <qnrgoqlt@naver.com>
2022-11-24 15:45:09 +0900

    헤더 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const header = () => {
-  return <div>이곳은 header입니다</div>;
+  return (
+    <>
+      <div className="root">
+        <div className="main">
+          <section className="mainListContainer">
+            <h2>오늘의 상품 추천</h2>
+            <div className="mainList"></div>
+          </section>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default header;
